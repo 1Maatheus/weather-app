e0a645b25c122b5d132070d9e615fb4e7e0b3870
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-11-10 01:07:53 -0300

    Customizing mobile

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 import apiKey from "./script/key.js";
 
+const input = document.querySelector(".input-search");
+const btn = document.querySelector(".btn-search");
+const cityName = document.querySelector(".cityName");
+const weatherIcon = document.querySelector(".weather-icon");
+const temperature = document.querySelector(".temperature");
+const wind = document.querySelector(".wind-velocity");
+const humidity = document.querySelector(".humidity");
+const content = document.querySelector(".main-content");
+
 const getWeather = async () => {
-  const city = "São Paulo";
+  const city = input.value;
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const response = await fetch(apiUrl);
   const data = await response.json();
 
+  cityName.innerHTML = data.name;
+  wind.innerHTML = data.wind.speed + "Km/h";
+  humidity.innerHTML = data.main.humidity + "%";
+  input.value = "";
+
+  content.classList.add("show");
+
   console.log(data);
 };
 
-getWeather();
+btn.addEventListener("click", () => {
+  getWeather();
+});
