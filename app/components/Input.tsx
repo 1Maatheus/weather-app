0161fe0cf1899d9b836f7b0087b0378770f2c4fa
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-01-31 20:55:24 -0300

    commit

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -3,16 +3,23 @@
 import React from "react";
 import { FaSearch } from "react-icons/fa";
 
-const Input = () => {
+type Props = {
+  handleSearch: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+  setLocation: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const Input = ({ handleSearch, setLocation }: Props) => {
   return (
     <form className="flex items-center md:w-2/4 w-full order-2 md:order-1">
       <input
+        onChange={(e) => setLocation(e.target.value)}
+        onKeyDown={handleSearch}
         className="p-2 w-full bg-transparent border-2 rounded-lg placeholder-white outline-none text-white"
         type="text"
-        placeholder="Cidade..."
+        placeholder="Insira o nome da cidade"
       />
       <div className="ml-[-25px] text-white cursor-pointer">
-        <FaSearch />
+        <FaSearch onClick={handleSearch} />
       </div>
     </form>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,35 @@
 "use client";
+
 import React from "react";
 import Input from "./components/Input";
 
 export default function Home() {
+  const [data, setData] = React.useState({});
+  const [location, setLocation] = React.useState("");
+  const [error, setError] = React.useState("");
+
+  const url = `http://api.weatherapi.com/v1/forecast.json?key=b169adcee3ed43d5ad7220536243101&q=${location}&days=7&aqi=yes&alerts=yes`;
+
+  const handleSearch = async (event: React.KeyboardEvent) => {
+    if (event.key === "Enter" || event.type === "click") {
+      event.preventDefault();
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error();
+        }
+        const data = await response.json();
+        setData(data);
+        setLocation("");
+        setError("");
+      } catch (error) {
+        setError("Cidade inválida.");
+        setData({});
+        setLocation("");
+      }
+    }
+  };
+
   return (
     <>
       <div
@@ -11,11 +38,13 @@ export default function Home() {
       >
         <div className="bg-white/25 w-full flex-col h-fit ">
           <div className="flex flex-col md:flex-row justify-between items-center p-12">
-            <Input />
+            <Input handleSearch={handleSearch} setLocation={setLocation} />
             <h1 className="mb-8 md:mb-0 order-1 text-white py-2 px-4 rounded-xl italic font-bold">
               Weather App.
             </h1>
           </div>
+
+          {data.current && <div>{data.current.temp_c}</div>}
         </div>
       </div>
     </>
